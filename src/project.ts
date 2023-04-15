5dfaeacebdea97354e4afd3b139c42154ed8b45f
K0dikas <kodikasYT@protonmail.com>
2023-04-15 21:17:41 +0800

    Added some files, for advance

--- src/project.ts
+++ src/project.ts
@@ -1,10 +1,13 @@
 import {makeProject} from '@motion-canvas/core';
 
 import Lucas from './scenes/Lucas?scene';
+import Melissa from './scenes/Melissa?scene';
 import Marion from './scenes/Marion?scene';
+import Tizon from './scenes/Tizon?scene';
+import Sianrey from './scenes/Sianrey?scene';
 import testing from './scenes/testing?scene';
 import './global.css';
 
 export default makeProject({
-  scenes: [Lucas, Marion, testing],
+  scenes: [Lucas, Melissa, Marion, Tizon, Sianrey, testing],
 });
