b21a5d311e2b6c3c43f964a74c419431f667d821
K0dikas <kodikasYT@protonmail.com>
2023-04-16 18:14:27 +0800

    Added the rest of the .tsx files

--- src/project.ts
+++ src/project.ts
@@ -5,9 +5,12 @@ import Melissa from './scenes/Melissa?scene';
 import Marion from './scenes/Marion?scene';
 import Tizon from './scenes/Tizon?scene';
 import Sianrey from './scenes/Sianrey?scene';
+import Abbygail from './scenes/Abbygail?scene';
+import Mardie from './scenes/Mardie?scene';
+import Pancho from './scenes/Pancho?scene';
 import testing from './scenes/testing?scene';
 import './global.css';
 
 export default makeProject({
-  scenes: [Lucas, Melissa, Marion, Tizon, Sianrey, testing],
+  scenes: [Lucas, Melissa, Marion, Tizon, Sianrey, Abbygail, Mardie, Pancho, testing],
 });
