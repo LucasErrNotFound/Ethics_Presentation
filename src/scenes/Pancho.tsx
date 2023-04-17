5415d1f2ad68c8a2717e31768ace6a507ae084d8
K0dikas <kodikasYT@protonmail.com>
2023-04-17 14:13:13 +0800

    Added the rest of the reference links

--- src/scenes/Pancho.tsx
+++ src/scenes/Pancho.tsx
@@ -1,19 +1,12 @@
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import {createSignal} from "@motion-canvas/core/lib/signals";
 import {Txt, Rect, Line, Circle, Spline, Knot, Img, Polygon, Node} from '@motion-canvas/2d/lib/components';
 import {beginSlide, createRef, makeRef, range} from '@motion-canvas/core/lib/utils';
 import {waitFor, waitUntil, all} from '@motion-canvas/core/lib/flow';
 import {easeInOutCubic, easeOutCubic, easeInCubic, easeInOutElastic, linear} from '@motion-canvas/core/lib/tweening';
-import {Vector2, Color} from '@motion-canvas/core/lib/types';
-import {Gradient} from '@motion-canvas/2d/lib/partials';
-import {CameraView} from "@ksassnowski/motion-canvas-camera";
-import {Gear, StrutConfig} from '@jtstrader/motion-canvas-components';
-import courage from "/src/scenes/Courage.png";
 import Helping1 from "/src/scenes/Helping_Man.svg.png";
 import Helping2 from "/src/scenes/Woman_Helping.svg";
 import Helping3 from "/src/scenes/Helping_3_Man.svg.png";
 import Helping4 from "/src/scenes/Cartoon_Helping_4.svg.png";
-import { CircleText } from "../components/CircleText";
 
 const BRIGHT_RED = "#fb4934"
 const BRIGHT_GREEN = "#b8bb26"
@@ -36,6 +29,8 @@ export default makeScene2D(function* (view){
 	const HELPING_4 = createRef<Img>();
 
 	const EXAMPLE_WORD = createRef<Txt>();
+
+	const REFERENCE_WORD = createRef<Txt>();
 
 	const REFERENCES_FORMAT = createRef<Node>();
 	const REFERENCE_1 = createRef<Txt>();
@@ -139,7 +134,7 @@ export default makeScene2D(function* (view){
 				fontFamily={'Fira Code Retina'}
 				fontSize={50}
 				lineHeight={80}
-				y={-350}
+				y={-120}
 			/>
 			<Txt
 				antialiased={true}
@@ -148,7 +143,8 @@ export default makeScene2D(function* (view){
 				fontFamily={'Fira Code Retina'}
 				fontSize={50}
 				lineHeight={80}
-				y={-350}
+				x={-40}
+				y={200}
 			/>
 			<Txt
 				antialiased={true}
@@ -158,7 +154,7 @@ export default makeScene2D(function* (view){
 				fontSize={50}
 				lineHeight={80}
 				x={-75}
-				y={350}
+				y={400}
 			/>
 			<Txt
 				antialiased={true}
@@ -167,7 +163,8 @@ export default makeScene2D(function* (view){
 				fontFamily={'Fira Code Retina'}
 				fontSize={50}
 				lineHeight={80}
-				y={-350}
+				x={-100}
+				y={-120}
 			/>
 			<Txt
 				antialiased={true}
@@ -176,7 +173,8 @@ export default makeScene2D(function* (view){
 				fontFamily={'Fira Code Retina'}
 				fontSize={50}
 				lineHeight={80}
-				y={-350}
+				x={0}
+				y={200}
 			/>
 			<Txt
 				antialiased={true}
@@ -217,6 +215,19 @@ export default makeScene2D(function* (view){
 		</Node>
 	);
 
+	view.add(
+		<Txt
+			antialiased={true}
+			ref={REFERENCE_WORD}
+			fontSize={150}
+			fontFamily={'Fira Code Retina'}
+			fill={BRIGHT_YELLOW}
+			text={`REFERENCE`}
+			x={0}
+			y={-400}
+		/>
+	);
+
 	yield* beginSlide('Tenth Slide');
 	yield* VIRTUE_IMPORTANCE().text("Why Being Virtuous Is Important?", 1);
 	yield* waitFor(13);
@@ -241,10 +252,31 @@ export default makeScene2D(function* (view){
 	yield* all(
 		REFERENCE_1().text(`1. https://books.google.com.ph/books?hl=en&lr=&id=s6jgBQAAQ\nBAJ&oi=fnd&pg=PP1&dq=virtue+ethics+meaning&ots=FXpizuK1lt&si\ng=TGsznn_0dnWiDO81uBBEB5n2Uqb8&redir_esc=y#v=onepage&q=virt\nue%20ethics%20meaning&f=false`, 3),
 
-		REFERENCE_3().text(`2. `, 3), // Mel
+		REFERENCE_3().text(`2. https://www.bbc.co.uk/ethics/introduction/virtue.shtml`, 3), // Mel
 		REFERENCE_4().text(`3. https://plato.stanford.edu/entries/aristotle-ethics/`, 3), // Marion
 
 	);
+	yield* waitFor(8);
 
-	yield* waitFor(10);
+	yield* all(
+		REFERENCE_1().opacity(0, 0.5),
+		REFERENCE_3().opacity(0, 0.5),
+		REFERENCE_4().opacity(0, 0.5),
+
+		REFERENCE_5().scale(1.3, 1),
+		REFERENCE_6().scale(1.2, 1),
+		REFERENCE_5().text(`4. 04_Handout_1(11).pdf (STI ELMS HANDOUT)`, 1),
+		REFERENCE_6().text(`5. https://www.linkedin.com/pulse/why-we-prioritise\n-virtues-over-values-our-work-lives-barrett-barrett`, 1),
+	);
+	yield* waitFor(8);
+
+	yield* all(
+		REFERENCE_WORD().position.y(0, 1, easeInOutCubic),
+		REFERENCE_WORD().scale(1.5, 1),
+		REFERENCE_WORD().text(`THANK YOU!`, 1),
+		REFERENCE_5().text(``, 1),
+		REFERENCE_6().text(``, 1),
+	);
+	yield* waitFor(5);
+	yield* REFERENCE_WORD().position.y(-1500, 1, easeInOutCubic);
 });
